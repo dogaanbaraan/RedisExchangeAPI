c15071315fa7c26b61b081ca03832ac7bf63d4e7
Baran Doğan <baran.dogan@mtteknoloji.com.tr>
2022-04-19 09:45:32 +0300

    StackExchangeRedis kütüphanesi eklendi, Redis Servera bağlantı yapıldı.

---
+++ RedisExchangeAPI.Web/Services/RedisService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Services
+{
+    public class RedisService
+    {
+
+        private readonly string _redisHost;
+        private readonly string _redisPort;
+
+        private IConnectionMultiplexer _redis;
+
+        public IDatabase db { get; set; }
+        public RedisService(IConfiguration configuration)
+        {
+            _redisHost = configuration["Redis:Host"];
+            _redisPort = configuration["Redis:Port"];
+        }
+
+        public void Connect()
+        {
+            var configString = $"{_redisHost} : {_redisPort}";
+            _redis = ConnectionMultiplexer.Connect(configString);   
+        }
+
+        public IDatabase GetDb(int db)
+        {
+            return _redis.GetDatabase(db);
+        }
+
+    }
+}
